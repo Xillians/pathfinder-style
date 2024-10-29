69c922ccf09777b1b980eb4ba57533c9be2f54bb
Xillians <sandra.toftemo@bjerk.io>
2024-10-29 19:20:57 +0100

    feat: implement svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let title = "App name";
+</script>
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<main>
+  <header>
+  </header>
+  <section class="container">
+    <slot></slot>
+  </section>
+  <footer>
+    <p>© 2021 Your Name</p>
+  </footer>
+</main>
+
+<style scoped>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<h1>Welcome to SvelteKit</h1>
+<div class="container">
+  <h2>
+    This is a h2
+  </h2>
+  <h3>
+    this is a h3
+  </h3>
+  <p>
+    This is a paragraph
+  </p>
+  <p>
+    This is another paragraph with a 
+    <a href="https://svelte.dev">
+      link
+    </a>
+  </p>
+  <b>
+    This is bold
+  </b>
+  <p class="hidden">
+    this text is hidden
+  </p>
+  <button class="primary">
+    Click me
+  </button>
+  <button class="secondary">
+    Click me
+  </button>
+</div>
